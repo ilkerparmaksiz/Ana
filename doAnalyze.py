3bad34782b84dfe42599deba26fa3640ed48dec6
Ilker Parmaksiz <ilkerparmaksiz@windowslive.com>
2020-03-30 14:05:16 -0500

    mksl.sh file editted

--- doAnalyze.py
+++ doAnalyze.py
@@ -28,14 +28,21 @@ FilePath="/home/ilker/Desktop/Analysis/Tests/Mothership_02_24_2020/"
 FilePath="/home/ilker/Desktop/Analysis/Second_Data/Nov_21_2019"
 
 def main():
-    ''' FilePath    =>  Either a path to root file or to root files
-        ThePath     =>  This is the Path to store analyzed and reconstructed files
-        Analyze     =>  Takes 1 or 0 (Activates or Deactivates Analysis)
-        Reconst     =>  Takes 1 or 0 (Activates or Deactivates Reconstruction)
-        pixelPath   =>  The Path to pixelization.txt
-        TablePath   =>  The Path to opRefTable.txt
+    ''' FilePath            =>  Either a path to root file or to root files
+        ThePath             =>  This is the Path to store analyzed and reconstructed files
+        Analyze             =>  Takes 1 or 0 (Activates or Deactivates Analysis)
+        Reconst             =>  Takes 1 or 0 (Activates or Deactivates Reconstruction)
+        pixelPath           =>  The Path to pixelization.txt
+        TablePath           =>  The Path to opRefTable.txt or splinedOpRefTable.txt
+        Radius              =>  Radius of the disk
+        sipmGainPath        =>  Path to the Gains of the SIPMs
+        SaveFile            =>  1 Saves the results to file, 0 is off
+        Cut                 =>  1 Applys the Cut
+        Th                  =>  ADC Value to apply the cut 220 around 1 PE
+        NofCoinc            =>  Max Number of SIPM coincidences
+        TrueFile            =>  TruePositions of the Files
     '''
-    MotherShip = {
+    MotherShip = {                                          # Variables to Use for Mothership Data
         "FilePath"          :   FilePath,
         "OutPath"           :   "output",
         "Analyze"           :   1,
@@ -50,10 +57,10 @@ def main():
         "NofCoinc"          :   4,
         "TrueFile"          :  "truePositions.txt"
     }
-    SmallWheel = {
+    SmallWheel = {                                          # Variables to Use for SmallWheel Data
         "FilePath"          :   FilePath,
         "OutPath"           :   "smallWheelOutput",
-        "Analyze"           :   0,
+        "Analyze"           :   1,
         "Reconst"           :   1,
         "pixelPath"         :   "production/5mm/pixelization.txt",
         "TablePath"         :   "production/5mm/16_sipm/splinedOpRefTable.txt",
--- mksl.sh
+++ mksl.sh
@@ -1,1 +1,3 @@
 g++ -I ${ROOTSYS}/include -std=c++14 -fPIC -shared Reconstructor.cxx -o libReconstructor.so
+mkdir -p output smallWheelOutput
+
